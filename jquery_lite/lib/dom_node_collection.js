92cff86d5cd88bf89ff7a59c730367399e22c3e4
Steven Choi <steven.m.choi@gmail.com>
2018-03-22 15:18:48 -0700

    Create html, empty, append, attr, addClass, and removeClass methods

--- jquery_lite/lib/dom_node_collection.js
+++ jquery_lite/lib/dom_node_collection.js
@@ -3,6 +3,65 @@ class DomNodeCollection {
     this.HTMLElementArr = HTMLElementArr;
   }
 
+  html(string) {
+    if (string || string === "") {
+      this.HTMLElementArr.forEach(el => el.innerHTML = string);
+    } else {
+      return this.HTMLElementArr[0].innerHTML;
+    }
+  }
+
+  empty() {
+    this.html("");
+  }
+
+  append(argument) {
+    switch (argument) {
+      case DomNodeCollection:
+        this.HTMLElementArr.forEach(thisEl => {
+          arguments.HTMLElementArr.forEach(argEl => thisEl.innerHTML.appendChild(argEl.outerHTML));
+        });
+        break;
+      case HTMLElement:
+        this.HTMLElementArr.forEach(el => el.innerHTML.appendChild(argument.outerHTML));
+        break;
+      default:
+        this.HTMLElementArr.forEach(el => el.innerHTML.appendChild(argument));
+    }
+  }
+
+  attr(attrName, value) {
+    if (value === undefined) {
+      return this.HTMLElementArr.find(el => el.attributes.name === attrName).value;
+    } else if (value === null) {
+      this.HTMLElementArr.forEach(el => {
+        el.removeAttribute(attrName);
+      })
+    } else {
+      this.HTMLElementArr.forEach(el => {
+        el.setAttribute(attrName, value);
+      });
+    }
+  }
+
+  addClass(className){
+    this.HTMLElementArr.forEach(el => {
+      let classes = el.getAttribute('class').split(' ');
+      classes.push(className);
+      el.setAttribute('class', classes.join(' '));
+    })
+  }
+
+  removeClass(className) {
+    this.HTMLElementArr.forEach(el => {
+      let classes = el.getAttribute('class').split(' ');
+      const index = classes.indexOf(className);
+      if (index > -1) {
+          classes.splice(index, 1);
+      }
+      el.setAttribute('class', classes.join(' '));
+    })
+  }
 }
 
 module.exports = DomNodeCollection;
